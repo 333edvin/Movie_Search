18feff4ededc268d4c9d4e2a81e894a6ddfea001
333edvin <127022159+333edvin@users.noreply.github.com>
2023-11-30 23:27:54 +0530

    poster

--- script.js
+++ script.js
@@ -12,19 +12,45 @@ try{
     console.log(data)
 
    innerdata = `
-   <div class="d-flex sec-res">
-   <h3>${data.Title} (${data.Year})</h3>
-   <div class="d-flex flex-column mx-5">
-   <h3>${data.Ratings[0].Value} <img src="./images/icons8-star.gif" style="width:25px;height:25px;margin-bottom:10px;"/></h3>
-   <p style="font-size:10px">${data.Ratings[0].Source}</p>
+   
+   <div class="container p-3">
+   <div class="row">
+       <div class="col-lg-3">
+           <img src="${data.Poster}" alt=""  class="img-fluid">
+           <p class="rounded border d-flex justify-content-center my-2">${data.Genre}</p>
+       </div>
+       <div class="col-lg-9 p-4">
+       <div class="d-flex">
+       <div>
+       <h3>${data.Title} (${data.Year})</h3>
+       <p style="font-size:12px">${data.Released}</p>
+       <div class="d-flex">
+       <p style="font-size:12px">${data.Language}-${data.Country}</p>
+       </div>
+       </div>
+
+       <div class="d-flex flex-column mx-5">
+       <h3>${data.Ratings[0].Value} <img src="./images/icons8-star.gif" style="width:25px;height:25px;margin-bottom:10px;"/></h3>
+       <p style="font-size:10px">${data.Ratings[0].Source}</p>
+       </div>
+       </div>
+       
+       <p><b>Plot :</b> ${data.Plot}</p>
+       <p><b>Cast : </b>${data.Actors}</p> 
+           <div>
+           <p><b>Director : </b>${data.Director}</p>
+           <p><b>Writer : </b>${data.Writer}</p>
+           </div>
+
+
+       </div>
    </div>
-   </div>
-                  <p><b>Plot:</b> <br>${data.Plot}</p> <br>
-                  <p><b>Director : </b>${data.Director}</p>
-                  <p><b>Actors : </b></b>${data.Actors}</p> 
+</div>
+   
                   `
 
-                  result.innerHTML = innerdata
+                  result.innerHTML = innerdata;
+                  
 }}
 catch(error){
 console.log(error);
